3e0577b900ff0dc96387a51af2002cf1890fe309
sroemersnappy <112185421+sroemersnappy@users.noreply.github.com>
2022-08-26 08:35:46 -0600

    Update GX06400.R

---
+++ GX06400.R
@@ -0,0 +1,40 @@
+## Savannah's feedback loop project
+#using R to create histograms similar to minitab
+#8/24/2022
+# Import packages
+library(SixSigma)
+library(readxl)
+library(units)
+FMax <- read_xlsx(file.choose(), col_names =FALSE, range = "C14:C68")
+FMax= as.matrix(FMax)
+
+
+
+# Type in measurements' values
+
+
+# Get the Z (sigma score) of the process
+ss.ca.z(FMax, LSL = 370, USL = 430)
+
+# Get the Cp indices of the process
+ss.ca.cp(FMax, LSL = 370, USL = 430)
+
+# Get the Cp indices confidence intervals
+ss.ca.cp(FMax, LSL = 370, USL = 430, ci = TRUE)
+
+# Get the Cpk indices of the process
+ss.ca.cpk(FMax, LSL = 370, USL = 430)
+
+# Get the Cpk indices confidence intervals
+ss.ca.cpk(FMax, LSL = 370, USL = 430, ci = TRUE)
+
+# Perform process capability analysis
+twopack(FMax,
+        LSL = 370,
+        USL = 430,
+        Target = 400,
+        alpha = 0.5,
+        f.sub = "Process capability")
+
+
+#hist(FMax, breaks=14)
