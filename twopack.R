a490a039a7bcc71aa1dd9d31a8235247e33f5432
sroemersnappy <112185421+sroemersnappy@users.noreply.github.com>
2022-08-30 11:20:29 -0600

    Update twopack.R

--- twopack.R
+++ twopack.R
@@ -12,12 +12,15 @@ library(ggplot2)
 file_user <- file.choose()
 
 #get the file path so that the PN and WO# can be extracted from it using basename
-extract=basename(file_user)
+extract =basename(file_user)
 
 my_string_split <- scan(text = extract, what = "")  # Apply scan function
+my_string_split2 <- scan(text = my_string_split[3], what = "", sep = ".")
 
 #concatenate substring one (part number) and substring two (WO#)
-PN_WO <- paste(my_string_split[1], my_string_split[2])
+PN_WO1 <- paste(my_string_split[1], my_string_split[2])
+PN_WO <- paste(PN_WO1, my_string_split2[1])
+
 
 #read Fmax data only from the excel file that was chosen
 FMax <- read_xlsx(file_user, col_names =FALSE, range = "C14:C68")
@@ -27,6 +30,18 @@ FMax= as.matrix(FMax)
 PN_data_xT_deduped <- read_excel("P:/Quality/Savannah/PN trip data/PN data xT deduped.xlsx")
                         
 index_of_PN <- which(PN_data_xT_deduped == my_string_split[1])
+
+# lookup of PN will fail if the PN includes something like "REV A"
+#Delete all of these occurrences so that only the PN remains
+
+
+withREV1 <- PN_data_xT_deduped[,1]
+withREV <- as.matrix(withREV1)
+
+for (i in 1:length(FMax)) parsesubstring <- scan(text = withREV[i,1], what = "")
+
+
+
 
 
 #  USL, LSL and Target values from the PN_data_xT_deduped excel file
